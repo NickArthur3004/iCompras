644c6cce59221f11b71b9e5e9b7ab6169aeeecff
NickArthur3004 <nicolas.arthur2786@gmail.com>
2026-04-21 10:58:12 +0100

    criando ms

---
+++ clientes/src/test/java/com/nickart/iservice/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickart.iservice.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
